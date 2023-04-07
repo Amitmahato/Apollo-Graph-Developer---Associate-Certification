e6bbdd8c0e7a1d0e59c6554aa884832c2138173b
Amit Mahato <amit@progressivelabs.tech>
2023-04-08 00:19:01 +0545

    feat: add resolver for incrementTrackViews  mutation query with intermediate value

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -20,6 +20,19 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(parent.id);
     },
   },
+
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      const track = await dataSources.trackAPI.incrementTrackViews(id);
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented the number of views for track ${id}`,
+        track,
+      };
+    },
+  },
 };
 
 module.exports = resolvers;
