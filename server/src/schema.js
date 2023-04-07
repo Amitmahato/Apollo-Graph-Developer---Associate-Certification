9202792cda43bb1cea98126d3d4ebff14e94ef7b
Amit Mahato <amit@progressivelabs.tech>
2023-04-07 23:47:07 +0545

    feat: add mutation type as an entry point - define incrmentTrackViews mutation method with custom response - define custom IncrementTrackViewResponse including three informations fields

--- server/src/schema.js
+++ server/src/schema.js
@@ -39,6 +39,21 @@ const typeDefs = gql`
     name: String!
     photo: String
   }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewResponse!
+  }
+
+  type IncrementTrackViewResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
 `;
 
 module.exports = { typeDefs };
